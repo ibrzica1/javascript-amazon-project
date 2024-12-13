7a4a4c4fe69cc605cebbfb97802ad834fc5e2f70
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2024-12-13 18:48:12 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,7 +81,15 @@ forEach((button)=>{
     });
   }
 
-    console.log(cart);
+  let cartQuantity = 0;
+
+  cart.forEach((item)=>{
+    cartQuantity+= item.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity').
+  innerHTML = cartQuantity;
+
   });
 });
 
